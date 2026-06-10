9e71e8ad76414257b2741828e10db6139761ae78
Daryl2002 <daryl@graduate.utm.my>
2026-06-10 14:16:20 +0800

    ar setup for kinect

--- Assets/Scripts/GladiatorSceneManager.cs
+++ Assets/Scripts/GladiatorSceneManager.cs
@@ -1,6 +1,16 @@
 using UnityEngine;
+using Photon.Pun;
 
-public class GladiatorSceneManager
+public class GladiatorSceneManager : MonoBehaviour
 {
-    
+    void Start()
+    {
+        Debug.Log("--- Gladiator Scene Loaded. Spawning Gladiator Avatar... ---");
+
+        // Spawns the Gladiator capsule at position (0, 0, 2) so they don't spawn right inside the Titan
+        if (PhotonNetwork.IsConnected)
+        {
+            PhotonNetwork.Instantiate("Net_Gladiator", new Vector3(0f, 0f, 2f), Quaternion.identity);
+        }
+    }
 }
--- Assets/Scripts/TitanSceneManager.cs
+++ Assets/Scripts/TitanSceneManager.cs
@@ -1,6 +1,17 @@
 using UnityEngine;
+using Photon.Pun;
 
-public class SceneTitanManager
+public class TitanSceneManager : MonoBehaviour
 {
-    
+    void Start()
+    {
+        Debug.Log("--- Titan Scene Loaded. Spawning Titan Avatar... ---");
+
+        // Spawns the Titan into the AR scene 
+        // Photon handles syncing this object across the network automatically
+        if (PhotonNetwork.IsConnected)
+        {
+            PhotonNetwork.Instantiate("Net_Titan", Vector3.zero, Quaternion.identity);
+        }
+    }
 }
