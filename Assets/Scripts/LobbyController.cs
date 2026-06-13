81eaa753cd0ee42cddb8741766fe7d54ed9f8090
rifatsyauqi1501 <muhammadrifatsyauqi@graduate.utm.my>
2026-06-13 13:34:47 +0800

    uodate masterclient

--- Assets/Scripts/LobbyController.cs
+++ Assets/Scripts/LobbyController.cs
@@ -1,41 +1,90 @@
 using UnityEngine;
 using Photon.Pun;
 using Photon.Realtime;
+using System.Collections;
 
 public class LobbyController : MonoBehaviourPunCallbacks
 {
+    private const string RoomName = "Colosseum";
+    private bool isVrPlayer;
+
     void Start()
     {
+        isVrPlayer = IsVrBuild();
+
         Debug.Log("--- PHOTON: Connecting to Server... ---");
-        // PhotonNetwork.AutomaticallySyncScene = true;
+        PhotonNetwork.AutomaticallySyncScene = false;
         PhotonNetwork.ConnectUsingSettings();
     }
 
     public override void OnConnectedToMaster()
     {
-        Debug.Log("--- PHOTON: Connected! Joining or creating room 'Colosseum'... ---");
-        RoomOptions roomOptions = new RoomOptions { MaxPlayers = 2 };
-        PhotonNetwork.JoinOrCreateRoom("Colosseum", roomOptions, TypedLobby.Default);
+        if (isVrPlayer)
+        {
+            Debug.Log("--- VR Player: Trying to join AR room... ---");
+            PhotonNetwork.JoinRoom(RoomName);
+        }
+        else
+        {
+            Debug.Log("--- AR Player: Creating or joining room as Player 1... ---");
+
+            RoomOptions roomOptions = new RoomOptions
+            {
+                MaxPlayers = 2
+            };
+
+            PhotonNetwork.JoinOrCreateRoom(RoomName, roomOptions, TypedLobby.Default);
+        }
     }
 
     public override void OnJoinedRoom()
     {
-        Debug.Log("--- PHOTON: Successfully entered room 'Colosseum'! ---");
-        Debug.Log("Players in room right now: " + PhotonNetwork.CurrentRoom.PlayerCount);
+        Debug.Log("--- Joined room: " + RoomName + " ---");
+        Debug.Log("Players in room: " + PhotonNetwork.CurrentRoom.PlayerCount);
+        Debug.Log("Is Master Client: " + PhotonNetwork.IsMasterClient);
 
-        #if UNITY_ANDROID || UNITY_IOS
-            Debug.Log("Mobile detected! Moving to VR Gladiator view...");
+        if (isVrPlayer)
+        {
+            Debug.Log("--- Loading VR Gladiator scene... ---");
             PhotonNetwork.LoadLevel("Scene_VR_Gladiator");
-        #else
-            Debug.Log("PC Desktop detected! Moving to AR Titan view...");
+        }
+        else
+        {
+            Debug.Log("--- Loading AR Titan scene... ---");
             PhotonNetwork.LoadLevel("Scene_AR_Titan");
-        #endif
+        }
+    }
+
+    public override void OnJoinRoomFailed(short returnCode, string message)
+    {
+        if (isVrPlayer)
+        {
+            Debug.LogWarning("--- VR Player: AR room not ready yet. Retrying... ---");
+            StartCoroutine(RetryJoinRoom());
+        }
+    }
+
+    IEnumerator RetryJoinRoom()
+    {
+        yield return new WaitForSeconds(2f);
+
+        if (PhotonNetwork.IsConnectedAndReady && !PhotonNetwork.InRoom)
+        {
+            PhotonNetwork.JoinRoom(RoomName);
+        }
     }
 
     public override void OnPlayerEnteredRoom(Player newPlayer)
     {
-        Debug.LogWarning("============== NETWORK ALERT ==============");
-        Debug.LogWarning("VR Gladiator (Player 2) has successfully connected to the room!");
-        Debug.LogWarning("===========================================");
+        Debug.Log("--- Player joined. Current player count: " + PhotonNetwork.CurrentRoom.PlayerCount + " ---");
+    }
+
+    bool IsVrBuild()
+    {
+#if UNITY_ANDROID || UNITY_IOS
+        return true;
+#else
+        return false;
+#endif
     }
 }
